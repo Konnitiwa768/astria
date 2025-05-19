75b15a9dbea35b02c2e48ec8311c030ccd459c07
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-20 07:45:17 +0900

    Update poseiditearmor.js

--- kubejs/server_scripts/armor/poseiditearmor.js
+++ kubejs/server_scripts/armor/poseiditearmor.js
@@ -46,7 +46,45 @@ function translate(part) {
     case 'helmet': return '兜'
     case 'chestplate': return '胸当て'
     case 'leggings': return '脚甲'
-    case 'boots': return '靴'
-    default: return ''
-  }
-}
+    cas// アーマーマテリアルを定義
+StartupEvents.registry('armor_material', event => {
+  event.create('poseidite')
+    .durabilityMultiplier(39) // ダイヤモンド相当
+    .slotProtections([4, 8, 6, 4]) // ヘルメット, チェスト, レギンス, ブーツ
+    .enchantmentValue(25) // エンチャント性（高め）
+    .equipSound('minecraft:item.armor.equip_diamond')
+    .repairIngredient('minecraft:prismarine_crystals')
+    .toughness(3.0) // タフネス（ネザライト相当）
+    .knockbackResistance(0.1) // 軽いノックバック耐性
+})
+
+// 防具アイテムを登録
+StartupEvents.registry('item', event => {
+  event.create('poseidite_helmet', 'armor')
+    .displayName('ポセイダイトの兜')
+    .armorType('helmet')
+    .armorMaterial('poseidite')
+    .texture('armors:models/armor/poseidite')
+    .rarity('uncommon')
+
+  event.create('poseidite_chestplate', 'armor')
+    .displayName('ポセイダイトの胸当て')
+    .armorType('chestplate')
+    .armorMaterial('poseidite')
+    .texture('armors:models/armor/poseidite')
+    .rarity('uncommon')
+
+  event.create('poseidite_leggings', 'armor')
+    .displayName('ポセイダイトの脚甲')
+    .armorType('leggings')
+    .armorMaterial('poseidite')
+    .texture('armors:models/armor/poseidite')
+    .rarity('uncommon')
+
+  event.create('poseidite_boots', 'armor')
+    .displayName('ポセイダイトの靴')
+    .armorType('boots')
+    .armorMaterial('poseidite')
+    .texture('armors:models/armor/poseidite')
+    .rarity('uncommon')
+})
