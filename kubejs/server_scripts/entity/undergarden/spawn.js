68af42aa343a0fba7f2b954434abe3ae271f0793
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-21 07:22:50 +0900

    Update spawn.js

--- kubejs/server_scripts/entity/undergarden/spawn.js
+++ kubejs/server_scripts/entity/undergarden/spawn.js
@@ -7,7 +7,7 @@ StartupEvents.registry('item', event => {
     .properties(p => p.food(() => null)) // 食べ物ではないことを明示
     .unstackable(false)
     .rarity('common')
-    .texture('kubejs:item/starfish_spawn_egg') // 任意のテクスチャを指定（後述）
+    .texture('custom:item/starfish_spawn_egg') // 任意のテクスチャを指定（後述）
     .tooltip('Spawns a Starfish')
     .spawnEgg('kubejs:starfish', 0xAA0000, 0xFF6666) // 赤ベース・薄赤スポット
 })
@@ -18,7 +18,7 @@ StartupEvents.registry('item', event => {
     .properties(p => p.food(() => null))
     .unstackable(false)
     .rarity('common')
-    .texture('kubejs:item/underrupter_spawn_egg')
+    .texture('custom:item/underrupter_spawn_egg')
     .tooltip('Spawns an Underrupter')
     .spawnEgg('kubejs:underrupter', 0x003300, 0x00AA00) // 黒緑ベース・黄緑スポット
 })
