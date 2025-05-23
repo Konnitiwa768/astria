1f32a41bc35155109cc374a627f90390acb5adb8
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-05-23 15:35:39 +0900

    Update apalachia_mob.js

--- kubejs/server_scripts/divine/apalachia/apalachia_mob.js
+++ kubejs/server_scripts/divine/apalachia/apalachia_mob.js
@@ -14,12 +14,12 @@ EntityEvents.spawned(event => {
     mob.getAttribute("minecraft:generic.attack_damage").baseValue = 22;
   }
   //  の調整
-  if (mob.type == 'divinerpg:apalachia_archer') {
-    mob.maxHealth = 108;
-    mob.health = 108;
+  if (mob.type == 'divinerpg:apalachia_tomo') {
+    mob.maxHealth = 122;
+    mob.health = 122;
 
     // 遠距離攻撃はprojectileに依存するので、簡易的には攻撃力属性を変更
-    mob.getAttribute("minecraft:generic.attack_damage").baseValue = 2;
+    mob.getAttribute("minecraft:generic.attack_damage").baseValue = 8;
 
     // より正確に調整するには発射物のカスタム処理が必要（別途対応可能）
   }
